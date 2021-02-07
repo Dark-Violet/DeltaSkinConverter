b847a3a32fcc6363aaf38b5aa36adceb52e28a30
Anonymous <>
2021-02-07 15:20:30 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DeltaSkinConverter
+
  Converts the old gba4ios skins into skins that are compatible with Delta.
---
+++ convert.py
@@ -0,0 +1,13 @@
+import easygui
+import zipfile
+
+# User selects the .gbaskin or .gbcskin file
+
+
+# Unzip the files into memory or tempfile.TemporaryDirectory
+
+
+# Convert the info.json file
+
+
+# Zip all files into a new .deltaskin
